2fa7537f8cb6c09d334e08b1dc1b0ef7e6c67870
Hariharan <hariharan@FULL169-AIR.FULL.Creative>
2018-04-05 03:46:13 +0530

    added scores

--- shuffler.js
+++ shuffler.js
@@ -17,24 +17,46 @@ function gameplay(computer, player){
 	let two = 'stone';
 	let three = 'scissor';
 
+	let elems = document.getElementsByClassName('score-user');
+
+	let userscoreelem;
+	let computerscoreelem;
+
+	for(let ele of elems){
+		if(ele.dataset.computerscore){
+			computerscoreelem = ele;
+		}
+
+		else if(ele.dataset.userscore){
+			userscoreelem = ele;
+		}
+
+	}
+
+
 	if(computer != player){
 
 		if(computer == one){
 			if(player == two){
+				computerscoreelem.innerText = "Computer -" + ++computerscoreelem.dataset.computerscore;
 				return one+" beats "+ two + " > computer wins";
 			}
 
 			else if(player == three){
+				userscoreelem.innerText = "You -" + ++userscoreelem.dataset.userscore;
 				return three+" beats "+ one + " > player wins";
 			}
 		}
 
 		else if(computer == two){
 			if(player == one){
+				//++userscoreelem.getAttribute('userscore');
+				userscoreelem.innerText = "You -" + ++userscoreelem.dataset.userscore;
 				return one+" beats "+ two + " > player wins";
 			}
 
 			else if(player == three){
+				computerscoreelem.innerText = "Computer -" + ++computerscoreelem.dataset.computerscore;
 				return two +" beats "+ three + " > computer wins";
 			}
 		}
@@ -42,10 +64,12 @@ function gameplay(computer, player){
 
 		else {
 			if(player == one){
+				computerscoreelem.innerText = "Computer -" + ++computerscoreelem.dataset.computerscore;
 				return three +" beats "+ one + " > computer wins";
 			}
 
 			else if(player == two){
+				userscoreelem.innerText = "You -" + ++userscoreelem.dataset.userscore;
 				return two +" beats "+ three + " > player wins";
 			}
 		}
