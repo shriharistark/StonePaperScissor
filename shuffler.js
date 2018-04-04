90e12db089d45d0528e89d341dc226160a73302a
Hariharan <hariharan@FULL169-AIR.FULL.Creative>
2018-04-05 00:38:39 +0530

    1st proto

--- shuffler.js
+++ shuffler.js
@@ -7,9 +7,52 @@ document.onload = function() {
 
 	let img = document.getElementById('computer-choice');
 	img.setAttribute("src",items[random]); 
+	img.setAttribute("value",items[random].slice(0,-4));
 	//console.log(document.getElementById('choice-shuffler').innerText);
 }
 
+function gameplay(computer, player){
+
+	let one = 'paper';
+	let two = 'stone';
+	let three = 'scissor';
+
+	if(computer != player){
+
+		if(computer == one){
+			if(player == two){
+				return one+" beats "+ two + " > computer wins";
+			}
+
+			else if(player == three){
+				return three+" beats "+ one + " > player wins";
+			}
+		}
+
+		else if(computer == two){
+			if(player == one){
+				return one+" beats "+ two + " > player wins";
+			}
+
+			else if(player == three){
+				return two +" beats "+ three + " > computer wins";
+			}
+		}
+
+
+		else {
+			if(player == one){
+				return three +" beats "+ one + " > computer wins";
+			}
+
+			else if(player == two){
+				return two +" beats "+ three + " > player wins";
+			}
+		}
+	}
+
+	return "both picked "+computer+". It is a tie";
+}
 
 function starthere(){
 	let choices = document.getElementsByClassName('user-choice-button');
@@ -25,13 +68,8 @@ function starthere(){
 		el.addEventListener('click', function(event){
 
 			clearInterval(athlete);
-			if((event.target.innerText+".png") == document.getElementById('computer-choice').src){
-				alert("you win");
-			}
-
-			else{
-				alert("try again loser");
-			}
+			console.log("player: "+ event.target.innerText + "| computer: " + document.getElementById('computer-choice').getAttribute('value'));
+			alert(gameplay(document.getElementById('computer-choice').getAttribute('value'), event.target.innerText));
 
 		});
 	}
